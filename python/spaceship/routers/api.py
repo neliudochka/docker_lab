78a582888a3c5632342feb52f5544cb347566a62
Mila <gorbunova.milasha@gmail.com>
2023-05-05 02:56:13 +0300

    Added matrix practice

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -6,3 +6,18 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/product')
+def matrix_product() -> dict:
+    import numpy as np
+
+    matrix1 = np.random.rand(10, 10)
+    matrix2 = np.random.rand(10, 10)
+
+    res = np.dot(matrix1, matrix2)
+
+    return {
+        'matrix1': matrix1.tolist(), 
+        'matrix2': matrix2.tolist(), 
+        'product': res.tolist()
+    }
